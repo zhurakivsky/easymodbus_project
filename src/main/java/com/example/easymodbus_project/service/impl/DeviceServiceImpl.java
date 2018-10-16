7331f1c693071e527a82adb65a329f6b59a67657
kvasnitsa <kvasnitsa09011995@gmail.com>
2018-10-16 11:04:54 +0300

    add modify to Devicedao "private"

---
+++ src/main/java/com/example/easymodbus_project/service/impl/DeviceServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.easymodbus_project.service.impl;
+
+import com.example.easymodbus_project.dao.DeviceDao;
+import com.example.easymodbus_project.model.Device;
+import com.example.easymodbus_project.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class DeviceServiceImpl implements DeviceService{
+    @Autowired
+    private DeviceDao deviceDao;
+    @Override
+    public Device findById(int id) {
+
+        return deviceDao.findById(id).get();
+    }
+
+    @Override
+    public List<Device> findAll() {
+        return deviceDao.findAll();
+    }
+
+    @Override
+    public void save(Device device) {
+        deviceDao.save(device);
+    }
+
+    @Override
+    public void delete(int id) {
+     deviceDao.deleteById(id);
+    }
+}
