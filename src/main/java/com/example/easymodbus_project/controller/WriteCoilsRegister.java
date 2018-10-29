826539b7873dcbce4b3f06094aae9afa0c429713
kvasnitsa <kvasnitsa09011995@gmail.com>
2018-10-29 11:14:08 +0200

    system workkkkkkk1

---
+++ src/main/java/com/example/easymodbus_project/controller/WriteCoilsRegister.java
@@ -0,0 +1,25 @@
+package com.example.easymodbus_project.controller;
+
+import de.re.easymodbus.exceptions.ModbusException;
+
+import java.io.IOException;
+
+public class WriteCoilsRegister {
+
+
+    public  static  void writeCoilsRegister(int address, boolean value){
+
+
+        try {
+            ModbusClientConnect.modbusClient.Connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ModbusClientConnect.modbusClient.WriteSingleCoil(address,value);
+        } catch (ModbusException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
